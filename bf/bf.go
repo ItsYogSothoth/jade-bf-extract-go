c3357de731942c7227edd1a87db74b730c1972bd
ItsYogSothoth <14014806+ItsYogSothoth@users.noreply.github.com>
2025-03-25 12:16:45 +0000

    fix index out of bounds exception when printing offset array

--- bf/bf.go
+++ bf/bf.go
@@ -170,8 +170,13 @@ Dir metadata table offset: %x
 
 func (bf *BigFile) PrintOffsetArray() {
 	fmt.Printf("index,file_data_offset,resouce_key,filename\n")
+	length := len(bf.Files)
 	for i := range len(bf.OffsetArray) {
-		fmt.Printf("%d,%x,%x,%s\n", i, bf.OffsetArray[i].FileDataOffset, bf.OffsetArray[i].ResourceKey, bf.Files[i].Metadata.Filename)
+		if i < length {
+			fmt.Printf("%d,%x,%x,%s\n", i, bf.OffsetArray[i].FileDataOffset, bf.OffsetArray[i].ResourceKey, bf.Files[i].Metadata.Filename)
+		} else {
+			fmt.Printf("%d,%x,%x,%s\n", i, bf.OffsetArray[i].FileDataOffset, bf.OffsetArray[i].ResourceKey, "<no-file>")
+		}
 	}
 }
 
